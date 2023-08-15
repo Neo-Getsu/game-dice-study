7a9ed03831ef531579cca76365cee8a807ed915a
Neo <dam.workit40@gmail.com>
2023-08-16 01:03:46 +0200

    Preparing js

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,7 @@
 //
 // This includes Popper and all of Bootstrap's JS plugins.
 
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
-//
-// Place any custom JS here
-//
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
+
